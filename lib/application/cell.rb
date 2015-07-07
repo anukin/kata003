641cabd9530e4b968c4e694af8d3f11a88e410aa
anukin <anukin2611@gmail.com>
2015-07-07 11:08:02 +0530

    Added functionality to the cell to correctly respond to state with DEAD or ALIVE

--- lib/application/cell.rb
+++ lib/application/cell.rb
@@ -1,6 +1,7 @@
 class Cell
   DEAD = Object.new
-
+  ALIVE = Object.new
+  
   def initialize(state)
     @state = state
   end
--- spec/application/cell_spec.rb
+++ spec/application/cell_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
 describe "Cell" do 
-  it "should respond to state " do 
+  it "should respond to state with DEAD when state is dead " do 
     cell = Cell.new(Cell::DEAD)
     expect(cell.state).to eq(Cell::DEAD)
+  end
+
+  it "should respond to state with ALIVE when state is alive" do 
+    cell = Cell.new(Cell::ALIVE)
+    expect(cell.state).to eq(Cell::ALIVE)
   end
 end
